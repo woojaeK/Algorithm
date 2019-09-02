618ce464e6e8c8da30bbb6cc6c13bf7692e878f1
woojae <woojae121@gmail.com>
2019-09-02 17:59:07 +0900

    0902

---
+++ Algo/src/D4/Solution_D4_1251_하나로_서울9반_김우재.java
@@ -0,0 +1,20 @@
+package D4;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_D4_1251_하나로_서울9반_김우재 {
+
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("res/input_D4_1251.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <T; tc++) {
+			 
+		}
+	}
+}
