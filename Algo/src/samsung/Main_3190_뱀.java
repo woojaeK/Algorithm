fa18b9bd4dbde922ba35c4d43253a49e83bc6346
woojae <woojae121@gmail.com>
2020-05-12 16:41:17 +0900

    뱀

--- Algo/src/samsung/Main_3190_뱀.java
+++ Algo/src/samsung/Main_3190_뱀.java
@@ -11,7 +11,7 @@ public class Main_3190_뱀 {
 	public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+         
         /*
         	종료 조건 : 벽에 부딪히거나 자기자신의 몸과 부딪힐경우
         	크기 : N, 사과의 개수 K, 첫번째 행 두번째 열  변환 횟수 L x초뒤 L은 왼쪽 R은 오른쪽 90도 방향
