0fe0d5e847e5359947074eaa730ecd227ce963f0
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-08-23 02:34:25 +0900

    20200822 k번째 수

--- Algo/src/programmers/K번째수.java
+++ Algo/src/programmers/K번째수.java
@@ -1,5 +1,25 @@
 package programmers;
 
-public class K번째수 {
+import java.util.*;
 
+public class K번째수 {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+
+		// i는 시작 j는 끝 정렬 후 -> k번째 수가 답
+		for (int i = 0; i < commands.length; i++) {
+			int a = commands[i][0];
+			int b = commands[i][1];
+			int c = commands[i][2];
+			int n = b - a + 1;
+			int[] list = new int[n];
+			for (int j = 0; j < n; j++) {
+				list[j] = array[a - 1 + j];
+			}
+
+			Arrays.sort(list);
+			answer[i] = list[c - 1];
+		}
+		return answer;
+	}
 }
