c484d0e6cd8a686aae8a91122a600e971c63225f
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-08-24 15:43:14 +0900

    20200824 가운데글자

---
+++ Algo/src/programmers/가운데글자.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class 가운데글자 {
+	public String solution(String s) {
+		String answer = "";
+		if (s.length() % 2 == 1) {
+			answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+		} else {
+			answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+		}
+		return answer;
+	}
+}
--- Algo/src/test.java
+++ Algo/src/test.java
@@ -2,21 +2,10 @@ import java.util.Arrays;
 
 public class test {
 	public static void main(String[] args) {
-		int[] a = { 3, 2, 6, 4, 5, 1 };
-		int n = a.length;
-
-		int[] l = new int[n];//
-
-		for (int i = 0; i < n; i++) {
-			l[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (a[j] < a[i] && l[i] < 1 + l[j]) {
-					l[i] = 1 + l[j];
-				}
-			}
-		}
-		System.out.println(Arrays.stream(l).max().getAsInt()); // 배열에서 최댓값 구하는 법
-		System.out.println(Arrays.toString(l));
+		int a = 15;
+		a =  a%7;
+		System.out.println(a);
+		String b = "";
+		b.substring(0);
 	}
-
 }
