ba97ef3c00e4c1a4cc35fe974a333de5a5741157
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-08-30 04:03:13 +0900

    20200830 약수의합

---
+++ Algo/src/programmers/약수의합.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class 약수의합 {
+	class Solution {
+	    public int solution(int n) {
+	        int answer = 0;
+	        
+	        for(int i = 1; i <= n/2; i++) {
+	            if(n % i == 0){
+	                System.out.println(i +"  " +answer);
+	                answer += i;
+	            }
+	        }
+	        answer += n;
+	        return answer;
+	    }
+	}
+}
