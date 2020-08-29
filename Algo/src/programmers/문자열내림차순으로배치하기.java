71af398b87a9b3237381c7a3e17bbbe8f8fe9e79
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-08-29 12:36:54 +0900

    20200829 문자열 내림차순으로 정렬하기

--- Algo/src/programmers/문자열내림차순으로배치하기.java
+++ Algo/src/programmers/문자열내림차순으로배치하기.java
@@ -1,5 +1,16 @@
 package programmers;
 
-public class 문자열내림차순으로배치하기 {
+import java.util.*;
 
+class 문자열내림차순으로배치하기 {
+    public String solution(String s) {
+        String answer = "";
+        String[] list = new String[s.length()];
+		list = s.split("");
+		Arrays.sort(list, Collections.reverseOrder());
+        for(int i = 0; i < s.length(); i++) {
+            answer += list[i];
+        }
+        return answer;
+    }
 }
