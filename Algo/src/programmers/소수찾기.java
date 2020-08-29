110c06760badd5e1732db0877afc78f65202b610
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-08-29 19:25:50 +0900

    20200829 소수찾기

--- Algo/src/programmers/소수찾기.java
+++ Algo/src/programmers/소수찾기.java
@@ -1,5 +1,22 @@
 package programmers;
 
-public class 소수찾기 {
-
+class 소수찾기 {
+    public int solution(int n) {
+        int answer = 0;
+        boolean chk[] = new boolean[n+1];
+        chk[0] = true; chk[1] = true;
+        
+        for(int i = 2; i < n+1; i++) {
+            if(!chk[i]) {
+                int a = 1;
+                answer++;
+                while(a*i <= n){
+                    chk[a*i] = true;
+                    a++;
+                }
+            }
+        }
+        
+        return answer;
+    }
 }
