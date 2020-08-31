c1871bfc7e38517df25901f9c9e5e4f3aeda6ff4
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-08-31 23:51:10 +0900

    20200831 이상한문자열만들기

---
+++ Algo/src/programmers/이상한문자열만들기.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+		String[] word = s.split(" ", -1);
+
+		for (int i = 0; i < word.length; i++) {
+			String a = word[i];
+			for (int j = 0; j < a.length(); j++) {
+				String b = Character.toString(a.charAt(j));
+				if (j % 2 == 1)	b = b.toLowerCase();
+				else b = b.toUpperCase();
+				answer += b;
+			}
+			if (word.length - 1 == i) break;
+			else answer += " ";
+		}        
+        return answer;
+    }
+}
