1f480cd81d4bacfeb5eaf5d4c673851935b65262
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-09-03 00:44:09 +0900

    20200902 키패드누르기

--- Algo/src/programmers/키패드누르기.java
+++ Algo/src/programmers/키패드누르기.java
@@ -1,51 +1,51 @@
 package programmers;
 
-public class 키패드누르기 {
+import java.util.*;
 
-	public static void main(String[] args) {
-		int[] numbers = {4,5,0};
-		String hand = "right";
+class 키패드누르기 {
+    
+    public String solution(int[] numbers, String hand) {
+	
 		if (hand.equals("right"))
 			hand = "R";
 		if (hand.equals("left"))
 			hand = "L";
 		String answer = "";
-		String[][] phone = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" }, { "*", "0", "#" } };
+		
 		int l1 = 3, l2 = 0, r1 = 3, r2 = 2;
 		for (int i = 0; i < numbers.length; i++) {
 			String s = Integer.toString(numbers[i]);
-			
 			if (s.equals("1")) {
 				answer += "L";
 				l1 = 0;
 				l2 = 0;
 			}
-			else if (s.equals("4")) {
+			if (s.equals("4")) {
 				answer += "L";
 				l1 = 1;
 				l2 = 0;
 			}
-			else if (s.equals("7")) {
+			if (s.equals("7")) {
 				answer += "L";
 				l1 = 2;
 				l2 = 0;
 			}
-			else if (s.equals("3")) {
+			if (s.equals("3")) {
 				answer += "R";
 				r1 = 0;
 				r2 = 2;
 			}
-			else if (s.equals("6")) {
+			if (s.equals("6")) {
 				answer += "R";
 				r1 = 1;
 				r2 = 2;
 			}
-			else if (s.equals("9")) {
+			if (s.equals("9")) {
 				answer += "R";
 				r1 = 2;
 				r2 = 2;
 			}
-			else if (s.equals("2")) {
+			if (s.equals("2")) {
 				int dir = check(l1, l2, r1, r2, 0, 1);
 				switch (dir) {
 				case 0:
@@ -53,7 +53,7 @@ public class 키패드누르기 {
 					if (hand.equals("L")) {
 						l1 = 0; l2 = 1; 
 					}
-					else if (hand.equals("R")) {
+					if (hand.equals("R")) {
 						r1 = 0; r2 = 1;
 					}
 					break;
@@ -69,7 +69,7 @@ public class 키패드누르기 {
 					break;
 				}
 			}
-			else if (s.equals("5")) {
+			if (s.equals("5")) {
 				int dir = check(l1, l2, r1, r2, 1, 1);
 				switch (dir) {
 				case 0:
@@ -77,7 +77,7 @@ public class 키패드누르기 {
 					if (hand.equals("L")) {
 						l1 = 1; l2 = 1; 
 					}
-					else if (hand.equals("R")) {
+					if (hand.equals("R")) {
 						r1 = 1; r2 = 1;
 					}
 					break;
@@ -93,7 +93,7 @@ public class 키패드누르기 {
 					break;
 				}
 			}
-			else if (s.equals("8")) {
+			if (s.equals("8")) {
 				int dir = check(l1, l2, r1, r2, 2, 1);
 				switch (dir) {
 				case 0:
@@ -101,7 +101,7 @@ public class 키패드누르기 {
 					if (hand.equals("L")) {
 						l1 = 2; l2 = 1; 
 					}
-					else if (hand.equals("R")) {
+					if (hand.equals("R")) {
 						r1 = 2; r2 = 1;
 					}
 					break;
@@ -117,7 +117,7 @@ public class 키패드누르기 {
 					break;
 				}
 			}
-			else if (s.equals("0")) {
+			if (s.equals("0")) {
 				int dir = check(l1, l2, r1, r2, 3, 1);
 				switch (dir) {
 				case 0:
@@ -125,7 +125,7 @@ public class 키패드누르기 {
 					if (hand.equals("L")) {
 						l1 = 3; l2 = 1; 
 					}
-					else if (hand.equals("R")) {
+					if (hand.equals("R")) {
 						r1 = 3; r2 = 1;
 					}
 					break;
@@ -142,15 +142,15 @@ public class 키패드누르기 {
 				}
 			}
 		}
-		System.out.println("{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}");
-		System.out.println("LRLLRRLLLRR");
-		System.out.println(answer);
-		// return answer;
+		//System.out.println("{ 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 }");
+		//System.out.println("LRLLLRLLRRL");
+		//System.out.println(answer);
+		return answer;
 	}
 
 	private static int check(int l1, int l2, int r1, int r2, int i, int j) {
-		double a = Math.pow(l1 - i, 2) + Math.pow(l2 - j, 2);
-		double b = Math.pow(r1 - i, 2) + Math.pow(r2 - j, 2);
+		double a = Math.abs(l1 - i) + Math.abs(l2 - j);
+		double b = Math.abs(r1 - i) + Math.abs(r2 - j);
 		int c = 0;
 		if (a > b)
 			c = 1;
