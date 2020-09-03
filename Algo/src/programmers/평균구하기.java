9fa420cfcbba1cae51e5e77ae2c5582914b56380
kimwoojae <36468699+woojaeK@users.noreply.github.com>
2020-09-03 22:04:36 +0900

    20200903 평균구하기

---
+++ Algo/src/programmers/평균구하기.java
@@ -0,0 +1,11 @@
+package programmers;
+
+public class 평균구하기 {
+	public double solution(int[] arr) {
+		double answer = 0;
+		for (int i = 0; i < arr.length; i++)
+			answer += arr[i];
+		answer /= arr.length;
+		return answer;
+	}
+}
